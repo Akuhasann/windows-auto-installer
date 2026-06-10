7e3dd7cd70e951c96b2a08e7dbfedc5b90c23960
Muhammad Hasan <100526388+Akuhasann@users.noreply.github.com>
2026-06-10 12:57:55 +0700

    Enhance clear.bat to remove empty directories
    Added folder cleanup for Windows Temp, User Temp, and Prefetch.

--- clear.bat
+++ clear.bat
@@ -24,24 +24,23 @@ echo.
 :: 1. Membersihkan Windows Temp
 echo [+] Membersihkan Windows Temp (C:\Windows\Temp)...
 del /f /s /q "C:\Windows\Temp\*" 2>nul
-rmdir /s /q "C:\Windows\Temp" 2>nul
-mkdir "C:\Windows\Temp" 2>nul
+:: Menghapus folder-folder kecil di dalamnya tanpa menghapus folder Temp utama
+for /d %%p in ("C:\Windows\Temp\*") do rmdir /s /q "%%p" 2>nul
 echo OK!
 echo ---------------------------------------------------
 
 :: 2. Membersihkan User Temp (%temp%)
 echo [+] Membersihkan User Temp (%USERPROFILE%\AppData\Local\Temp)...
 del /f /s /q "%temp%\*" 2>nul
-rmdir /s /q "%temp%" 2>nul
-mkdir "%temp%" 2>nul
+:: Menghapus folder-folder kecil di dalamnya tanpa menghapus folder Temp utama
+for /d %%p in ("%temp%\*") do rmdir /s /q "%%p" 2>nul
 echo OK!
 echo ---------------------------------------------------
 
 :: 3. Membersihkan Prefetch
 echo [+] Membersihkan Prefetch (C:\Windows\Prefetch)...
 del /f /s /q "C:\Windows\Prefetch\*" 2>nul
-rmdir /s /q "C:\Windows\Prefetch" 2>nul
-mkdir "C:\Windows\Prefetch" 2>nul
+for /d %%p in ("C:\Windows\Prefetch\*") do rmdir /s /q "%%p" 2>nul
 echo OK!
 echo ---------------------------------------------------
 
